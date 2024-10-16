71594888f80a4efa0ad20f59d5d71b9f9898434b
Sadhgun2018 <71647133+Sadhgun2018@users.noreply.github.com>
2024-10-17 02:23:18 +0530

    Create HooksUI.cs

---
+++ HooksUI.cs
@@ -0,0 +1,49 @@
+[Binding]
+
+[TestFixture]
+public class LoginTests:BaseclassUI
+
+[Setup]
+public enum BrowserType
+{
+  Chrome,
+  Firefox,
+  Edge,
+  Safari
+}
+
+private readonly ScenarioContext _scenarioContext;
+private BrowserType _browserType;
+public Browser(ScenarioContect scenarioContext)
+{
+  _scenarioContext = ScenarioContext;
+  _browserType = (BrowserType)Enum.Parse(typeof(BrowserType)),
+  ConfigurationManager.AppSetting["Browser"];
+}
+
+[BeforeScenario]
+public void Setup() 
+{
+  switch (_browserType)
+  {
+      case BrowserType.Chrome:
+      _driver = new ChromeDriver();
+      break;
+      
+      case BrowserType.Firefox:
+      _driver = new FireFoxDriver();
+      break;
+
+      case BrowserType.Edge:
+      _driver = new EdgeDriver();
+      break;
+
+      case BrowserType.Safari:
+      _driver = new SafariDriver();
+      break;
+  }
+}
+
+
+
+
