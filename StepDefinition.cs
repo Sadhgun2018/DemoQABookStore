753dde50546ff7dce6987a069716fb958460f63f
Sadhgun2018 <71647133+Sadhgun2018@users.noreply.github.com>
2024-10-17 10:21:13 +0530

    Create StepDefinition.cs

---
+++ StepDefinition.cs
@@ -0,0 +1,13 @@
+[Binding]
+public class LoginSteps
+{
+   private LoginPage _loginPage
+   {
+      _loginPage = new LoginPage;
+   }
+  [Given (@"User Access the BookStoreURL "(.*)")]
+          public void GivenUserAccesstheBookStoreURL (string url)
+          {
+            
+          }
+}
