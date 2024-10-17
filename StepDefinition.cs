76c0e759a3f1a430a4c711e1695be24b1247ee9a
Sadhgun2018 <71647133+Sadhgun2018@users.noreply.github.com>
2024-10-17 11:31:18 +0530

    Update StepDefinition.cs

--- StepDefinition.cs
+++ StepDefinition.cs
@@ -1,7 +1,10 @@
 [Binding]
 public class LoginSteps
 {  
-   private IWebDriver driver;
+   private IWebDriver driver;   
+   private RestClient client;
+   private RestRequest request;
+   private IRestResponse response;
    private Userservice _UserService;
    private IRestResponse _response;
    private LoginPage _loginPage
@@ -41,13 +44,13 @@ public class LoginSteps
           }
 
 
-[Given (@"User landed on FormPage")
+[Given (@"User landed on FormPage")]
     public void GivenUserlandedonFormPage
     {
        Objects.FormBanner();
     }
  
-When User Enter User details
+ [When (@"User Enter User details")]
    public void WhenUserEnterUserdetails(table Table)
     {
     Dictionaty <string, string> UserDetails = new Dictionary <string, string>(Table);
@@ -61,10 +64,58 @@ When User Enter User details
     }
  
 
-And User click on Submit Button
-Then System popsup as successfully Submitted
+[When (@"User click on Submit Button")]
+ public void WhenUserclickonSubmitButton(IWebElement element)
+ {
+    Objects.element.Click();
+ }
+ 
+[Then (@"System popsup as successfully Submitted")]
+    public void ThenSystempopsupassuccessfullySubmitted(string massage)
+    {
+       Assert.AreEqual("successfully Submitted", massage);
+    }
 
-    
+
+
+      public CreateUserSteps(string URL_API)
+        {
+            client = new RestClient(URL_API);
+        }
+
+   [Given(@"I have the user details")]
+        public void GivenIHaveTheUserDetails()
+        {            
+            request = new RestRequest("users", Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+
+            var body = new
+            {
+                name = "Ram Varma",
+                email = "RamVarma@gmail.com",
+                password = "password123"
+            };
+            request.AddJsonBody(body);
+        }
+
+   [When(@"I send a request to create a new user")]
+        public void WhenISendARequestToCreateANewUser()
+        {
+            response = client.Execute(request);
+        }
+
+   [Then(@"I should get a response with status code (.*)")]
+        public void ThenIShouldGetAResponseWithStatusCode(int statusCode)
+        {
+            Assert.AreEqual((HttpStatusCode)statusCode, response.StatusCode);
+        }
+
+   [Then(@"the response should contain the user information")]
+        public void ThenTheResponseShouldContainTheUserInformation()
+        {
+            Assert.IsTrue(response.Content.Contains("Ram Varma"));
+            Assert.IsTrue(response.Content.Contains("RamVarma@gmail.com"));
+        }
           
          
 }
